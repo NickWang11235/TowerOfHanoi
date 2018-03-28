fe73d233c39d4212501c3d139dab3f3d738592c4
cccc1 <cccc1@192.168.0.36>
2018-03-27 19:08:49 -0700

    Merge origin/master
    Conflicts:
    	src/towerofhanoi/TowerOfHanoi.java

---
+++ src/towerofhanoi/Test.java
@@ -0,0 +1,13 @@
+package towerofhanoi;
+
+public class Test {
+    
+    public static void main(String[] args){
+        
+        TowerOfHanoi test = new TowerOfHanoi(8);
+        
+        test.printCurrentState();
+        
+    }
+    
+}
