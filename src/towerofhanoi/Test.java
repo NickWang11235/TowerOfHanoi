e5a28eee5c77417b363a3a52cf04b331c14aef82
cccc1 <cccc1@192.168.0.36>
2018-03-27 19:40:23 -0700

    Merge origin/master
    Conflicts:
    	src/towerofhanoi/TowerOfHanoi.java

--- src/towerofhanoi/Test.java
+++ src/towerofhanoi/Test.java
@@ -4,10 +4,11 @@ public class Test {
     
     public static void main(String[] args){
         
-        TowerOfHanoi test = new TowerOfHanoi(8);
+        TowerOfHanoi test = new TowerOfHanoi(2);
+        System.out.println("Starting board :");
+        test.print();
         
-        test.printCurrentState();
-        
+        test.solveBaseCase();
     }
     
 }
