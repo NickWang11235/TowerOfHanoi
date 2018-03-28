8724d8a6573ca070ec52fa367bc4a283421b397a
cccc1 <cccc1@192.168.0.36>
2018-03-27 21:04:16 -0700

    Merge origin/master
    Conflicts:
    	src/towerofhanoi/TowerOfHanoi.java

--- src/towerofhanoi/TowerOfHanoi.java
+++ src/towerofhanoi/TowerOfHanoi.java
@@ -19,23 +19,53 @@ public class TowerOfHanoi {
         for(int i = 1; i <= n; i++){
             start.add(i);
         }
+        
     }
+    
+    /**
+     * Move the top element to the designated list.
+     * @param from target ArrayList
+     * @param to designated ArrayList
+     */
+    public static void moveTo(ArrayList from, ArrayList to){
+        
+        to.add(from.get(from.size()-1));
+        from.remove(from.size()-1);
 
+    }
+    
+    
+    /**
+     * To solve the simplest case of 2 pieces.
+     */
+    public void solveBaseCase(){
+        
+        moveTo(start, mid);
+        print();
+        moveTo(start, end);
+        print();
+        moveTo(mid, end);
+        print();
+        
+    }
+    
     /**
      * To recursively solve the puzzle
      */
     public void solve(){
+        
+        
         
     }
     
     /**
      * To print out the current state of the puzzle.
      */
-    public void printCurrentState(){
+    public void print(){
         
-        System.out.println(start);
-        System.out.println(mid);
-        System.out.println(end);
+        System.out.println("start :" + start);
+        System.out.println("Mid :" + mid);
+        System.out.printf("End :" + end +"\n\n");
         
     }
     
